6fbf7c13ac078a979f1880e9643d1bae590fe091
Kingdon Barrett <kingdon.b@nd.edu>
2018-05-06 14:45:17 -0400

    temporary change defaults
    smaller cluster size for smoke testing only
    
    (what's a clusterator AUTH_TOKEN and where does it come from?)

--- jobs/clusterator_jobs.groovy
+++ jobs/clusterator_jobs.groovy
@@ -16,9 +16,9 @@ job("clusterator-create") {
 
   parameters {
     stringParam('NUMBER_OF_CLUSTERS', '1', 'Number of clusters to create at 1 time')
-    stringParam('NUM_NODES', '5', 'Number of nodes in each cluster')
-    stringParam('MACHINE_TYPE', 'n1-standard-4', 'Node type')
-    stringParam('VERSION', '1.8.8', 'The version of kubernetes to use.')
+    stringParam('NUM_NODES', '3', 'Number of nodes in each cluster')
+    stringParam('MACHINE_TYPE', 'n1-standard-1', 'Node type')
+    stringParam('VERSION', '1.8.12', 'The version of kubernetes to use.')
   }
 
   wrappers {
